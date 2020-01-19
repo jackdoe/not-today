84ccab37ea96ec8485e35ddfc78bcafdf109b885
borislav nikolov <jack@sofialondonmoskva.com>
2020-01-19 13:08:37 +0100

    exec script

--- main.go
+++ main.go
@@ -2,19 +2,15 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"flag"
 	"fmt"
-	"github.com/sfreiberg/gotwilio"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"strings"
+	"os/exec"
 	"sync"
 	"time"
 )
-
-const MIN_NOTIFICATION_INTERVAL_MS = int64(3600000)
 
 type Failure struct {
 	Error   string
@@ -22,22 +18,50 @@ type Failure struct {
 	StampMs int64
 }
 
+func (f *Failure) JSON() string {
+	b, err := json.Marshal(f)
+	if err != nil {
+		panic(err)
+	}
+	return string(b)
+}
+
 type HealthCheck struct {
 	URL                string
 	Failures           []*Failure
-	notify             []string
 	LastSuccessMs      int64
 	LastFailureMs      int64
 	LastNotificationMs int64
+	Alive              bool
 	sync.Mutex
 }
 
-func NewHealthCheck(url string, notify []string) *HealthCheck {
+func (h *HealthCheck) JSON() string {
+	b, err := json.Marshal(h)
+	if err != nil {
+		panic(err)
+	}
+	return string(b)
+}
+
+func run(command string, args ...string) {
+	log.Printf("running: %v %v", command, args)
+
+	cmd := exec.Command(command, args...)
+	b, err := cmd.CombinedOutput()
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("output: " + string(b))
+}
+
+func NewHealthCheck(url string, interval int, cmd string) *HealthCheck {
 	check := &HealthCheck{
 		URL:                url,
 		Failures:           []*Failure{},
 		LastNotificationMs: 0,
-		notify:             notify,
+		Alive:              true,
 	}
 
 	go func() {
@@ -46,8 +70,7 @@ func NewHealthCheck(url string, notify []string) *HealthCheck {
 		}
 
 		client := &http.Client{Transport: tr, Timeout: 10 * time.Second}
-		fixed := true
-		go check.send(fmt.Sprintf("%s - started monitoring", check.URL))
+		log.Printf("checking %v", check.URL)
 		for {
 			t0 := nowMs()
 			resp, err := client.Get(check.URL)
@@ -55,9 +78,10 @@ func NewHealthCheck(url string, notify []string) *HealthCheck {
 				_, err = ioutil.ReadAll(resp.Body)
 			}
 			if err == nil && resp.StatusCode != 200 {
-				err = errors.New(fmt.Sprintf("expected status code %d, but got %d", 200, resp.StatusCode))
+				err = fmt.Errorf("%s expected status code %d, but got %d", check.URL, 200, resp.StatusCode)
 			}
-			if resp != nil && resp.Body != nil {
+
+			if resp.Body != nil {
 				resp.Body.Close()
 			}
 			if err != nil {
@@ -73,68 +97,48 @@ func NewHealthCheck(url string, notify []string) *HealthCheck {
 				}
 				check.Unlock()
 
-				if t0-check.LastNotificationMs > MIN_NOTIFICATION_INTERVAL_MS && fixed {
-					check.LastNotificationMs = t0
-					go check.send(fmt.Sprintf("%s - failed, took: %d, error: %s", check.URL, failure.TookMs, err.Error()))
+				check.LastNotificationMs = t0
+
+				if check.Alive {
+					go run(cmd, "false", err.Error())
 				}
-				fixed = false
+
+				check.Alive = false
 				check.LastFailureMs = t0
 			} else {
 				check.LastSuccessMs = t0
-				fixed = true
+				if !check.Alive {
+					go run(cmd, "true", "")
+				}
+				check.Alive = true
+
 			}
-			time.Sleep(60 * time.Second)
+			time.Sleep(time.Duration(interval) * time.Second)
 		}
 	}()
 	return check
-}
-
-func (this *HealthCheck) send(message string) {
-	for _, number := range this.notify {
-		sendMessage(number, message)
-	}
 }
 
 func nowMs() int64 {
 	return time.Now().UnixNano() / int64(time.Millisecond)
 }
 
-type TwilioAccount struct {
-	Sid    string
-	Token  string
-	Number string
-}
-
-var TWILIO = TwilioAccount{}
-
-func sendMessage(to, message string) {
-	log.Printf("sending to %s : %s", to, message)
-	gotwilio.NewTwilioClient(TWILIO.Sid, TWILIO.Token).SendSMS(TWILIO.Number, to, message, "", "")
-
-}
-
 func main() {
-	var ptoken = flag.String("token", "", "twilio auth token from https://www.twilio.com/console")
-	var psid = flag.String("sid", "", "twilio account sid from https://www.twilio.com/console")
-	var pnumber = flag.String("number", "", "twilio phone number from https://www.twilio.com/console/phone-numbers/incoming")
+	var pexec = flag.String("cmd", "", "exec this command when healthcheck fails")
 	var pbind = flag.String("bind", ":8080", "ListenAndServe argument")
-	var punparsedChecks = flag.String("checks", "", "comma separated list of health checks, for example: https://neko.science/@+123123123,https://google.com@+123123")
+	var pinterval = flag.Int("interval", 60, "run checks every N seconds")
 	flag.Parse()
-	if *ptoken == "" || *psid == "" || *pnumber == "" {
-		log.Fatalf("expected -token -sid and -number, use -help to see all options")
+	if len(flag.Args()) == 0 {
+		log.Fatal("pass urls after the arguments, e.g.: go run main.go -exec /tmp/bad.sh http://google.com http://yahoo.com")
 	}
-	TWILIO.Sid = *psid
-	TWILIO.Number = *pnumber
-	TWILIO.Token = *ptoken
+
+	if *pexec == "" {
+		log.Fatalf("expected -exec command that will be executed when healthcheck fails")
+	}
 
 	checks := []*HealthCheck{}
-	for _, site := range strings.Split(*punparsedChecks, ",") {
-		splitted := strings.Split(site, "@")
-		if len(splitted) == 1 {
-			log.Fatalf("expected phone number to notify: %s", site)
-		}
-		url, notify := splitted[0], splitted[1:]
-		checks = append(checks, NewHealthCheck(url, notify))
+	for _, site := range flag.Args() {
+		checks = append(checks, NewHealthCheck(site, *pinterval, *pexec))
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -143,7 +147,7 @@ func main() {
 			w.WriteHeader(http.StatusInternalServerError)
 		} else {
 			w.Header().Set("Content-Type", "application/json")
-			w.Write(b)
+			_, _ = w.Write(b)
 		}
 	})
 
